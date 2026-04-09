b8487b82a88adee7f419df99e9b5531e74706285
oyejin0925 <oyejin@catholic.ac.kr>
2026-04-09 14:38:31 +0900

    commit

---
+++ src/main/java/com/example/my_api_server/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.example.my_api_server.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/health")
+public class HealthCheckController { //ALB가 헬스 체크하는 엔드포인트
+    @GetMapping
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("연결 성공1");
+    }
+}
